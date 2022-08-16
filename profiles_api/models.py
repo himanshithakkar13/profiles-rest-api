bdda54c3ace7d1a503680fd1af5a79947e577fec
Himanshi <Himanshithakkar76@gmail.com>
2022-08-16 15:38:22 +0530

    Added HelloApiView

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -12,7 +12,8 @@ class UserProfileManager(BaseUserManager):
         user=self.model(email=email,name=name)
         user.set_password(password)
         user.save(using=self._db)
-    def create_super_user(self,email,name,password):
+        return user
+    def create_superuser(self,email,name,password):
         "Create and save a new super user with given details"
         user=self.create_user(email,name,password)
         user.is_superuser=True
@@ -24,6 +25,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     """ Database model for users in the system"""
     email=models.EmailField(max_length=255,unique=True)
     name=models.CharField(max_length=255)
+    is_superuser=models.BooleanField(default=False)
     is_active=models.BooleanField(default=True)
     is_staff=models.BooleanField(default=False)
     
